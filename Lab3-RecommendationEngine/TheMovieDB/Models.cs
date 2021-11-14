30f534088a463b198f56b287b42ccbad5d214a01
s15468 <s15468@pjwstk.edu.pl>
2021-11-14 01:57:48 +0100

    Implemented TheMovieDBApiService
    Implemented api to get expected movie description

---
+++ Lab3-RecommendationEngine/TheMovieDB/Models.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Lab3_RecommendationEngine.TheMovieDB
+{
+    public class TMDBMovie
+    {
+        public string overview { get; set; }
+        public string title { get; set; }
+    }
+
+    public class TMDBMovies
+    {
+        public List<TMDBMovie> results { get; set; }
+    }
+}
---
+++ Lab3-RecommendationEngine/TheMovieDB/TheMovieDBApiService.cs
@@ -0,0 +1,47 @@
+﻿using Lab3_RecommendationEngine.TheMovieDB;
+using RestSharp;
+using RestSharp.Serialization.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab3_RecommendationEngine.REST
+{
+    public class TheMovieDBApiService
+    {
+        private const string BASE_URL = @"https://api.themoviedb.org/3/";
+        private const string API_KEY = @"api_key=957d8e820a52252469f1d3b08fc2f59c";
+        private const string PARAMETERS = @"search/movie?";
+        
+        private readonly IRestClient _client;
+
+        public TheMovieDBApiService()
+        {
+            _client = new RestClient(BASE_URL + PARAMETERS + API_KEY);
+        }
+
+        public string Execute(string movieName)
+        {
+            IRestRequest request = new RestRequest();
+            request.AddParameter("query", movieName);
+            IRestResponse response = _client.Execute(request);
+
+            if (response.ErrorException != null)
+            {
+                const string message = "Error retrieving response. Check inner details for more information";
+                throw new Exception(message, response.ErrorException);
+            }
+
+            TMDBMovies tmdbMovies = new JsonDeserializer().Deserialize<TMDBMovies>(response);
+
+            TMDBMovie movie = tmdbMovies.results.FirstOrDefault(x => x.title.Equals(movieName, StringComparison.OrdinalIgnoreCase));
+
+            if (movie == null)
+            {
+                return "Unable to find description of selected movie in external API";
+            }
+
+            return movie.overview;
+        }
+    }
+}
