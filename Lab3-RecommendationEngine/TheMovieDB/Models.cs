35b12dd8c8bf72e95097c863f1e9159fbffbd3ff
s15468 <s15468@pjwstk.edu.pl>
2021-11-14 02:33:04 +0100

    Fixed model for External API response parser
    Fixed model by adding name to find tv series and media_type to sort only movies and tv series.

--- Lab3-RecommendationEngine/TheMovieDB/Models.cs
+++ Lab3-RecommendationEngine/TheMovieDB/Models.cs
@@ -4,8 +4,10 @@ namespace Lab3_RecommendationEngine.TheMovieDB
 {
     public class TMDBMovie
     {
+        public string name { get; set; }
         public string overview { get; set; }
         public string title { get; set; }
+        public string media_type { get; set; }
     }
 
     public class TMDBMovies
--- Lab3-RecommendationEngine/TheMovieDB/TheMovieDBApiService.cs
+++ Lab3-RecommendationEngine/TheMovieDB/TheMovieDBApiService.cs
@@ -11,7 +11,7 @@ namespace Lab3_RecommendationEngine.REST
     {
         private const string BASE_URL = @"https://api.themoviedb.org/3/";
         private const string API_KEY = @"api_key=957d8e820a52252469f1d3b08fc2f59c";
-        private const string PARAMETERS = @"search/movie?";
+        private const string PARAMETERS = @"search/multi?";
         
         private readonly IRestClient _client;
 
@@ -20,7 +20,7 @@ namespace Lab3_RecommendationEngine.REST
             _client = new RestClient(BASE_URL + PARAMETERS + API_KEY);
         }
 
-        public string Execute(string movieName)
+        public TMDBMovie Execute(string movieName)
         {
             IRestRequest request = new RestRequest();
             request.AddParameter("query", movieName);
@@ -33,15 +33,36 @@ namespace Lab3_RecommendationEngine.REST
             }
 
             TMDBMovies tmdbMovies = new JsonDeserializer().Deserialize<TMDBMovies>(response);
+            IEnumerable<TMDBMovie> foundMovies = tmdbMovies.results.Where(x => isMediaTypeValid(x) && isNameOrTitleValid(x));
+            TMDBMovie movie = foundMovies.FirstOrDefault(x => isNameOrTitleEqualsExpectedName(x, movieName));
 
-            TMDBMovie movie = tmdbMovies.results.FirstOrDefault(x => x.title.Equals(movieName, StringComparison.OrdinalIgnoreCase));
-
-            if (movie == null)
+            if (movie == null || movie.overview == null)
             {
-                return "Unable to find description of selected movie in external API";
+                Console.WriteLine("Unable to find description of selected movie in external API");
             }
 
-            return movie.overview;
+            return movie;
+        }
+
+        private bool isMediaTypeValid(TMDBMovie movie)
+            => movie.media_type.Contains("movie") || movie.media_type.Contains("tv");
+
+        private bool isNameOrTitleValid(TMDBMovie movie)
+            => movie.name != null || movie.title != null;
+
+        private bool isNameOrTitleEqualsExpectedName(TMDBMovie movie, string expectedName)
+        {
+            if (movie.name != null && movie.name.Equals(expectedName, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+
+            if (movie.title != null && movie.title.Equals(expectedName, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
