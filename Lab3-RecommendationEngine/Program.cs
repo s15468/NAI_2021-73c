5878cb084e75b44703af63bdd7b25169329b52d5
s15468 <s15468@pjwstk.edu.pl>
2021-11-19 01:07:17 +0100

    Refactor main program class to be soo simply

--- Lab3-RecommendationEngine/Program.cs
+++ Lab3-RecommendationEngine/Program.cs
@@ -1,22 +1,10 @@
-﻿using Lab3_RecommendationEngine.Database;
-using Lab3_RecommendationEngine.Recommendation;
-using Lab3_RecommendationEngine.REST;
-using System.Collections.Generic;
-
-namespace Lab3_RecommendationEngine
+﻿namespace Lab3_RecommendationEngine
 {
     class Program
     {
         static void Main(string[] args)
         {
-            MenuService menuService = new MenuService();
-            TheMovieDBApiService TMDBApiService = new TheMovieDBApiService();
-            
-            IEnumerable<User> allUsers = new DatabaseService().GetUsers();
-            User currentUser = menuService.SelectUserToRecommendMovies(allUsers);
-            RecommendationService recommendationService = new RecommendationService(currentUser, allUsers);
-
-            recommendationService.CalculateScore();
+            new MenuService().MainMenu();
         }
     }
 }
