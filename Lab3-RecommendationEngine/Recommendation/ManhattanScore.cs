d70eee33138e18351da83eb95abe32ecc435acee
s15468 <s15468@pjwstk.edu.pl>
2021-11-19 01:07:31 +0100

    Implemented ManhattanScore

---
+++ Lab3-RecommendationEngine/Recommendation/ManhattanScore.cs
@@ -0,0 +1,31 @@
+﻿using Lab3_RecommendationEngine.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab3_RecommendationEngine.Recommendation
+{
+    public class ManhattanScore : IComputeScore
+    {
+        public double CalculateScore(RecommendationUserData userData, User currentUser)
+        {
+            List<double> diffRatings = new();
+
+            foreach (Movie equalMovie in userData.EqualsMovies)
+            {
+                int currentUserRating = currentUser.Movie.First(x => x.Title == equalMovie.Title).Rating;
+                diffRatings.Add(Math.Abs(currentUserRating - equalMovie.Rating));
+            }
+
+            if (diffRatings.Count() == 0)
+            {
+                return 100;
+            }
+
+            return getAlgorithmScore(diffRatings);
+        }
+
+        private double getAlgorithmScore(List<double> diffRatings)
+            => diffRatings.Sum();
+    }
+}
