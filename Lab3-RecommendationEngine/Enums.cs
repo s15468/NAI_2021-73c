7441c6e058f940cb2d5cbe6d08a236446731a1c3
s15468 <s15468@pjwstk.edu.pl>
2021-11-19 01:07:01 +0100

    Created Enums for application Menu logic

---
+++ Lab3-RecommendationEngine/Enums.cs
@@ -0,0 +1,23 @@
+﻿namespace Lab3_RecommendationEngine
+{
+    public enum AlgorithmType
+    {
+        Euclidean,
+        Manhattan,
+        None,
+    }
+
+    public enum RecommendationType
+    {
+        Best,
+        Worst,
+        None,
+    }
+
+    public enum Option
+    {
+        Action,
+        ExitApp,
+        Return,
+    }
+}
