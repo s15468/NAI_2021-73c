f7753aa17b1ce4525c52d3fcd57f5566c7dc295d
s15468 <s15468@pjwstk.edu.pl>
2021-12-03 22:51:24 +0100

    Implemented RenderService to render for user messages in console.

---
+++ Lab4-SvmClassification/RenderService.cs
@@ -0,0 +1,37 @@
+﻿namespace Lab4_SvmClassification
+{
+    /// <summary>
+    /// Class to render message in console for user.
+    /// </summary>
+    public class RenderService
+    {
+        /// <summary>
+        /// Method to render menu with option to select.
+        /// </summary>
+        /// <param name="svmList">List of implemented Svm classes.</param>
+        public void RenderSelectSvmMenu(IEnumerable<ISvmClassification> svmList)
+        {
+            int counter = 0;
+
+            Console.WriteLine("Select SVM type to classificate data:");
+
+            foreach (ISvmClassification svmType in svmList)
+            {
+                Console.WriteLine($"{counter} {svmType.GetType().Name}");
+                counter++;
+            }
+
+            Console.WriteLine("4 Exit application");
+        }
+
+        /// <summary>
+        /// Method to render warning message when user input incorrect value.
+        /// </summary>
+        public void WrongUserInputMessage()
+        {
+            Console.WriteLine("Input is incorrect. Click any button to try again.");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
