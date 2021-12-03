f7d85e98ebe8fcc2eee220f69347fce2a30244ed
s15468 <s15468@pjwstk.edu.pl>
2021-12-03 22:51:46 +0100

    Implemented Interface for future implementation of SVM.

---
+++ Lab4-SvmClassification/SvmClassification/ISvmClassification.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab4_SvmClassification
+{
+    /// <summary>
+    /// Public interface for future implementation of Svm Classificaiton types.
+    /// </summary>
+    public interface ISvmClassification
+    {
+        /// <summary>
+        /// Method to invoke expected Svm type.
+        /// </summary>
+        void Invoke();
+    }
+}
