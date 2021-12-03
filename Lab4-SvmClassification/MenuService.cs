7c75c77367a0e5a39886b77176b5024f1d7d2196
s15468 <s15468@pjwstk.edu.pl>
2021-12-03 22:51:10 +0100

    Created MenuService to communicate with user to select SVM.

---
+++ Lab4-SvmClassification/MenuService.cs
@@ -0,0 +1,47 @@
+﻿namespace Lab4_SvmClassification
+{
+    /// <summary>
+    /// Method to show user main menu of application.
+    /// </summary>
+    public class MenuService
+    {
+        private readonly RenderService _renderService;
+
+        /// <summary>
+        /// Default constructor of class with initialize fields.
+        /// </summary>
+        public MenuService()
+        {
+            _renderService = new RenderService();
+        }
+
+        /// <summary>
+        /// Method to get from use selected Svm Classification type.
+        /// </summary>
+        /// <param name="smvList">List of Svm Classificaiton types.</param>
+        /// <returns></returns>
+        public char SelectSvmType(IEnumerable<ISvmClassification> smvList)
+        {
+            char input;
+
+            do
+            {
+                _renderService.RenderSelectSvmMenu(smvList);
+                input = Console.ReadKey().KeyChar;
+
+                switch (input)
+                {
+                    case '0':
+                    case '1':
+                    case '4':
+                        return input;
+
+                    default:
+                        _renderService.WrongUserInputMessage();
+                        break;
+                }
+            }
+            while (true);
+        }
+    }
+}
